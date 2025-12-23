fbd03b1fdb15f29340ab7cb558ed9653e9de5069
leejiyoul <gyoul1123@naver.com>
2025-12-23 16:09:07 +0900

    day1

---
+++ c_class_vaction4/기본구조.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hello World! 안녕하세요 저는 이지율\n");
+	printf("오늘은 월요일입니다.");
+	printf("수빈\n 다연\n\n 루다\n 제리\n"); 
+	printf("강아지\t고양이\t토끼\n");
+	printf("dog\tcat\trabbit\n");
+	printf("12345678\t1234\n");
+	printf("강아지는 \"멍멍\" 고양이는 \"야옹\" 토끼는 몰라");
+	return 0;
+}
